8deae9c99a0b76c218e4e520992439cbab04ed7d
Leon <mazleon777@gmail.com>
2025-06-15 21:59:13 +0300

    allom

--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,7 +1,8 @@
-export type VoteType = 'good' | 'neutral' | 'bad';
+export const Votes = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
-export interface Votes {
-  good: number;
-  neutral: number;
-  bad: number;
-}
+export type VoteType = keyof typeof Votes;
+export type Votes = typeof Votes;
